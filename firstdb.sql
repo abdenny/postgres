c20b4e641eb4fd79916939591046cc9f3c7221e4
Austin Denny <austinbdenny@gmail.com>
2020-03-09 13:18:13 -0500

    first commit.

---
+++ firstdb.sql
@@ -0,0 +1,25 @@
+
+-- CREATE TABLE student (
+--     name varchar,
+--     website varchar,
+--     github_username varchar, 
+--     points integer,
+--     gender char(1), 
+--     cohort_start_date date,
+--     graduated boolean
+-- );
+
+-- CREATE TABLE student2 (
+--     id SERIAL NOT NULL PRIMARY KEY,
+--     name varchar,
+--     website varchar,
+--     github_username varchar, 
+--     points integer,
+--     gender char(1), 
+--     cohort_start_date date,
+--     graduated boolean
+-- );
+
+INSERT INTO student2 VALUES (
+    DEFAULT, 'Austin', 'austindenny.me', 'abdenny', 5, 'M', '2020-03-09', FALSE
+);
